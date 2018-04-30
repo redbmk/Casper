e1b5d346814e7c783347d55f110f1e735c9c7de5
Braden M. Kelley <redbmk@gmail.com>
2018-04-30 14:33:48 -0700

    fix author bugs, grouping improperly

--- assets/js/author.js
+++ assets/js/author.js
@@ -12,92 +12,59 @@ jQuery(($) => {
         const [, authorSlug] = /^\/author\/(.*?)\//.exec(window.location.pathname) || [];
 
         if (!authorSlug) return;
+        const authorFilter = { slug: authorSlug };
 
         const { posts } = await $.getJSON(ghost.url.api('posts', { include: 'authors,tags', limit: 'all' }));
 
-        posts.forEach(post => {
-            const text = $(post.html.replace(/<img .*?>/g, '')).text().trim();
-            post.excerpt = text.split(/\s+/).slice(0, 33).join(' ');
-            post.isProject = !!_.find(post.tags, { slug: 'projects' });
-            post.hasProject = !!_.find(post.tags, { meta_title: 'project' });
-            post.hasClient = !!_.find(post.tags, { meta_title: 'client' });
-        });
-
         const tags = _.uniqBy(_.flatten(posts.map(post => post.tags)), 'id');
+        const projects = _.remove(posts, post => post.tags.find(tag => tag.slug === 'projects'));
+        const clientsById = _.filter(tags, { meta_title: 'client' }).reduce((collection, tag) => {
+            collection[tag.id] = { tag, projects: [] };
+            return collection;
+        }, {});
 
-        const projects = _.remove(posts, 'isProject');
-        const clients = _.filter(tags, { meta_title: 'client' });
+        const authorPosts = posts.filter(post => _.find(post.authors, authorFilter));
 
-        clients.push({ id: 0, name: 'No Client' });
+        projects.forEach((project) => {
+            project.posts = [];
 
-        const byClient = new Map();
-
-        const postsWithoutProject = posts.filter(post => !post.hasProject);
-        const individualPosts = postsWithoutProject
-            .filter(post => !post.hasClient && _.find(post.authors, { slug: authorSlug }));
-        const postUrlsToKeep = _.groupBy(individualPosts, 'url');
-
-        let numPosts = individualPosts.length;
-        let numProjects = 0;
-        let numClients = 0;
-
-
-        clients.forEach(clientTag => {
-            let clientProjects;
-            if (clientTag.id) {
-                const findClient = ({ tags }) => _.find(tags, { id: clientTag.id });
-                clientProjects = [
-                    ...projects.filter(findClient),
-                    ...postsWithoutProject.filter(findClient),
-                ];
-            } else {
-                const projectTags = _.countBy(_.flatten(projects.map(getProjectTags)), 'id');
-                const hasProjectTag = post => getProjectTags(post).find(tag => projectTags[tag.id]);
-
-                clientProjects = [
-                    ...projects,
-                    ...posts.filter(post => post.hasProject && !hasProjectTag(post)),
-                ].filter(post => !post.hasClient);
-            }
-
-            const byProject = new Map();
-
-            clientProjects.forEach(project => {
-                const projectTags = project.isProject ? getProjectTags(project) : [];
-                const tagIds = _.groupBy(projectTags, 'id');
-                const projectPosts = posts
-                    .filter(post => post.tags.find(tag => tagIds[tag.id]));
-
-                const authors = _.flatten([
-                    project.authors || [],
-                    ...projectPosts.map(post => post.authors),
-                ]);
-
-                project.project_tag = projectTags[0];
-                project.authors = _.uniqBy(authors, 'id');
-
-                if (_.find(project.authors, { slug: authorSlug })) {
-                    if (project.isProject) {
-                        numProjects++;
-                    } else {
-                        numPosts++;
-                    }
-
-                    numPosts += projectPosts.length;
-                    byProject.set(project, projectPosts);
+            project.tags.forEach((tag) => {
+                if (tag.meta_title === 'client') {
+                    clientsById[tag.id].projects.push(project);
+                } else if (tag.meta_title === 'project') {
+                    project.posts = _.uniq([
+                        ...project.posts,
+                        ...authorPosts.filter(post => _.find(post.tags, { id: tag.id })),
+                    ]);
                 }
             });
 
-            if (byProject.size) {
-                if (clientTag.id) numClients++;
-                byClient.set(clientTag, Array.from(byProject));
-            }
+            project.shouldShow = project.posts.length || !!_.find(project.authors, authorFilter);
         });
 
+        const clients = Object.values(clientsById).map((client) => {
+            const projects = _.filter(client.projects, 'shouldShow');
+            return projects.length ? { ...client.tag, projects } : null;
+        }).filter(Boolean);
+
+        const filteredProjects = _.uniq(_.flatten(clients.map(({ projects }) => projects)));
+
+        [...filteredProjects, ...authorPosts].forEach(post => {
+            const text = $(post.html.replace(/<img .*?>/g, '')).text().trim();
+            post.excerpt = text.split(/\s+/).slice(0, 33).join(' ');
+        });
+
+        const individualPosts = [...authorPosts];
+        filteredProjects.forEach(({ posts }) => {
+            posts.forEach(({ id }) => _.remove(individualPosts, { id }));
+        });
+
+        const postUrlsToKeep = _.groupBy(individualPosts, 'url');
+
         $('.author-stats').html('');
-        updateStats('post', numPosts);
-        updateStats('project', numProjects);
-        updateStats('client', numClients);
+        updateStats('post', authorPosts.length);
+        updateStats('project', filteredProjects.length);
+        updateStats('client', clients.length);
 
         const template = await $.get('/assets/html/authors.html', { bust: +new Date() });
 
@@ -110,7 +77,7 @@ jQuery(($) => {
         new Vue({
             el: '#app',
             data: {
-                clients: Array.from(byClient),
+                clients,
                 hasExtraPosts: individualPosts.length > 0,
             },
         });
